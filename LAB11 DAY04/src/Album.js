f53912868ebec94b750eea148f3e105cf13d919d
ImZocke <91361456+ImZocke@users.noreply.github.com>
2022-12-30 23:54:13 +0800

    Add files via upload

---
+++ LAB11 DAY04/src/Album.js
@@ -0,0 +1,193 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Button from '@mui/material/Button';
+import CameraIcon from '@mui/icons-material/PhotoCamera';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {database} from './firebase'
+import {ref, push, child, update, set, getDatabase, onValue, query, limitToLast} from "firebase/database";
+import * as firebase from './firebase';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+function ListItem(props) {
+  return <li>{props.post}</li>;
+}
+
+class Post extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      post: null,
+      posts: [],
+    };
+  }
+
+  /* TODO: finish this method */
+  getPosts = () => {
+    const recentPostsRef = query(ref(database, 'posts'), limitToLast(100));
+    onValue(recentPostsRef, (snapshot) => {
+       let newPosts = []
+       snapshot.forEach((childSnapshot) => {
+         /* TODO: parse the childSnapshot and use newPosts.push() to store the key and the post pair. You can use console.log() first to see what childSnapshot looks like. */
+         const k = childSnapshot.key;
+         const p = childSnapshot.val().post;
+         newPosts.push({k, p})
+       });
+       this.setState({posts: newPosts})
+     }, {
+       onlyOnce: true
+     });
+  }
+
+  componentDidMount() {
+    /* TODO: call the method to get posts */
+    this.getPosts();
+  }
+
+  handleChange = (e) => {
+     const {name , value} = e.target;
+     if (name == 'post') {
+        this.state.post = value
+     }
+  }
+
+  handleSubmit = (e) => {
+    let obj = {
+      post : this.state.post,
+    }
+    // Create a unique key for new posts
+    const newPostKey = push(child(ref(database), 'posts')).key;
+    const updates = {};
+    updates['/posts/' + newPostKey] = obj;
+    update(ref(database), updates);
+
+    /* TODO: call the method to get the posts */
+    this.getPosts();
+    e.preventDefault()
+  };
+}
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const theme = createTheme();
+
+export default function Album() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <CameraIcon sx={{ mr: 2 }} />
+          <Typography variant="h6" color="inherit" noWrap>
+            Posts layout
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <Box
+          sx={{
+            bgcolor: 'background.paper',
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              Posts
+            </Typography>
+            <Typography variant="h5" align="center" color="text.secondary" paragraph>
+              See below for all posts.
+            </Typography>
+            <Stack
+              sx={{ pt: 4 }}
+              direction="row"
+              spacing={2}
+              justifyContent="center"
+            >
+            </Stack>
+          </Container>
+        </Box>
+        <Container sx={{ py: 8 }} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                >
+                  <CardMedia
+                    component="img"
+                    sx={{
+                      // 16:9
+                      pt: '0',
+                    }}
+                    image="https://source.unsplash.com/random"
+                    alt="random"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      recentPostsRef
+                    </Typography>
+                    <Typography>
+                      Tolol
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">View</Button>
+                    <Button size="small">Edit</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          Something here to give the footer a purpose!
+        </Typography>
+        <Copyright />
+      </Box>
+      {/* End footer */}
+    </ThemeProvider>
+  );
+}
